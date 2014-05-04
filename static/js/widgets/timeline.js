6d89b3d178d856324dc4a5adbcd61d3c015c3bda
Wolf Bergenheim <wolf@bergenheim.net>
2014-05-04 16:38:12 +0200

    Styling of timeline + dynamic trip duration.

--- static/js/widgets/timeline.js
+++ static/js/widgets/timeline.js
@@ -1,6 +1,5 @@
 function TripManager(parent, infocard, trips)
 {
-    this.infocard = infocard;
     this.trips = trips;
     this.selected_trip = null;
     this.timeline = new Timeline(parent);
@@ -8,8 +7,8 @@ function TripManager(parent, infocard, trips)
 
 TripManager.prototype.listTrips = function()
 {
-    this.infocard.close();
-    this.infocard.setHeader('My trips');
+    infocard.close();
+    infocard.setHeader('My trips');
     var ul = $('<ul>');
     for (i in this.trips) {
         var trip = this.trips[i];
@@ -23,31 +22,32 @@ TripManager.prototype.listTrips = function()
         .html('Create a new trip');
     var li = $('<li>').append(a).appendTo(ul);
 
-    this.infocard.setContents(ul);
-    this.infocard.open();
+    infocard.setContents(ul);
+    infocard.open();
 };
 
 TripManager.prototype.createTrip = function()
 {
-    this.infocard.open();
+    infocard.open();
 };
 
 TripManager.prototype.showTrip = function(trip_id)
 {
+    var tripmanager = this;
     $.getJSON( "/widgets/timeline/" + trip_id, function( trip ) {
         if(trip) {
-            this.selected_trip = trip;
-            this.infocard.close();
-            this.timeline.setHeader(trip.name);
-            this.timeline.clearLocations();
+            tripmanager.selected_trip = trip;
+            infocard.close();
+            tripmanager.timeline.setHeader(trip.name);
+            tripmanager.timeline.clearLocations();
             for(i in trip.locations) {
-                location = trip.locations[i];
-                this.timeline.addLocation(location);
+                var place = trip.locations[i];
+                tripmanager.timeline.addLocation(place);
             }
-            this.timeline.popup();
+            tripmanager.timeline.popup();
         }
         else {
-            alert("failed to get trip details for trip " + trip);
+            alert("failed to get trip details for trip " + trip_id);
         }
     });
 };
@@ -57,7 +57,7 @@ function Timeline(parent)
     this.div = 
         $('<div>', {
             id: "timeline",
-            class: "gradual popdown"
+            class: "gradual"
         })
         .mouseenter(this, function(e) {
             e.data.popup();
@@ -65,53 +65,72 @@ function Timeline(parent)
         .click(this, function(e) {
             e.data.popdown();
         })
-        .css('bottom', '-35px')
+        .css('bottom', '-70px')
         .appendTo(parent);
+    this.line = $('<hr>').appendTo(this.div);
     this.title = $('<h3>').appendTo(this.div);
-    this.timeline = $('<div>').appendTo(this.div);
+    this.timeline = $('<div>', {
+        id: "timeline-labels"
+    }).appendTo(this.div);
     this.destinations = [];
+    this.trip_duration = 0;
 }
 
 Timeline.prototype.popup = function()
 {
     this.draw();
-    $(this.div).css('bottom','0px');
+    $(this.div).css('bottom','22px');
     // draw lines to the map
 };
 
 Timeline.prototype.popdown = function()
 {
-    $(this.div).css('bottom','-35px');
-    this.timeline.html='';
+    $(this.div).css('bottom','-80px');
+    this.timeline.empty();
 };
 
 Timeline.prototype.setHeader = function(newHeader)
 {
-    this.header.html = newHeader;
+    this.title.html(newHeader);
 };
 
-Timeline.prototype.addLocation = function(location)
+Timeline.prototype.addLocation = function(place)
 {
-    this.destinations.append(location);
+    console.log("adding location " + place.name);
+    this.destinations.push(place);
+    this.trip_duration += place.duration * 60; /* trip duration in minutes */
 };
 
-Timeline.prototype.clearLocations = function(location)
+Timeline.prototype.clearLocations = function()
 {
     this.destinations = [];
+    this.timeline.empty();
+    this.trip_duration = 0;
 };
 
 Timeline.prototype.draw = function()
 {
-    this.timeline.html = '';
-    for(i in self.destinations) {
-        var location = self.destinations[i];
-        var div = $('<div>');
-        $('<i>').appendTo(div);
-        $('<strong>')
-            .html(location.name)
+    this.timeline.empty();
+    var timeline_width = this.div.width() - this.title.outerWidth();
+    
+    var left_space = this.title.outerWidth();
+    for(i in this.destinations) {
+        if((i > 0) && (this.trip_duration != 0)) { // space comes after
+            console.log("trip time: " + this.destinations[i-1].duration);
+            console.log("timeline_width: " + timeline_width);
+            console.log("trip duration: " + this.trip_duration);
+            left_space += Math.floor((this.destinations[i-1].duration * 60) *
+                         (timeline_width/this.trip_duration));
+        }
+        var place = this.destinations[i];
+        console.log(place.name + " left_space: " + left_space + "px");
+        var div = $('<div>').css('left',left_space + 'px');
+        var i = $('<i>').appendTo(div);
+        var strong = $('<strong>')
+            .html(place.name)
             .appendTo(div);
-        $('<small>')
-            .html(location.date + ' - ' + location.duration + 'days')
+        var small = $('<small>')
+            .html(place.date + ' ' + place.duration + '&nbsp;days')
             .appendTo(div);
         $(div).appendTo(this.timeline);
     }
--- widgets/timeline.py
+++ widgets/timeline.py
@@ -2,6 +2,9 @@
 
 he timeline is a trip that a user takes."""
 
+import json
+import datetime
+
 import flask
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -13,14 +16,15 @@ class Location(ndb.Model):
     duration = ndb.IntegerProperty()
 
 class Travel(ndb.Model):
-    mode = ndb.StringProperty(choices=["car", "boat", "plane", "other"])
+    mode = ndb.StringProperty(
+        choices=["car", "boat", "plane", "train", "other"])
     origin = ndb.GeoPtProperty()
     destination = ndb.GeoPtProperty()
     travel_time = ndb.FloatProperty()
 
 class Trip(ndb.Model):
     name = ndb.StringProperty()
-    owner = ndb.UserProperty()
+    owner = ndb.StringProperty()
     locations = ndb.StructuredProperty(Location, repeated=True)
     travel = ndb.StructuredProperty(Travel, repeated=True)
 
@@ -29,28 +33,43 @@ class Timeline(object):
         self.user = users.get_current_user()
         self.app = app
         if self.user:
-            self.trips = Trip.query(Trip.owner == user)
+            self.trips = Trip.query(Trip.owner == self.user.email())
         else:
             self.trips = []
 
     def render(self, trip_id=None):
         if trip_id:
             # require the user to own the trip
-            trip = Trip.get_by_id(trip_id)
-            if trip and self.user and self.user.email == trip.owner:
-                trip = trip.to_dict()
+            trip = ndb.Key(urlsafe=trip_id).get()
+            if trip and self.user and self.user.email() == trip.owner:
+                trip = json.loads(
+                    json.dumps(trip.to_dict(), cls=NdbJSONEncoder))
             else:
                 trip = {}
             return flask.jsonify(**trip)
         else:
             trips = []
             for trip in self.trips:
-                trips.append(trip.to_dict())
+                trips.append({'name': trip.name, 'key': trip.key.urlsafe()})
             widget = {
                 'name': 'timeline',
-                'js': flask.url_for('static', filename='js/widgets/timeline.js'),
-                'css': flask.url_for('static', filename='css/widgets/timeline.css'),
+                'js': flask.url_for('static',
+                                    filename='js/widgets/timeline.js'),
+                'css': flask.url_for('static',
+                                     filename='css/widgets/timeline.css'),
                 'trips': trips,
             }
             return flask.jsonify(**widget)
 
+
+class NdbJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        # If this is a key, you might want to grab the actual model.
+        if isinstance(o, ndb.Model):
+            return o.to_dict
+        elif isinstance(o, ndb.GeoPt):
+            return (o.lat, o.lon)
+        elif isinstance(o, (ndb.Key,datetime.date, datetime.datetime)):
+            return str(o)  # Or whatever other date format you're OK with...
+        else:
+            return json.JSONEncoder.default(o)
