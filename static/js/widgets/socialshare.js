25d95994606248b333e96f91f9fc884e28fa1685
Wolf Bergenheim <wolf@bergenheim.net>
2014-05-18 13:23:26 +0200

    Fixed social buttons.

--- static/js/widgets/socialshare.js
+++ static/js/widgets/socialshare.js
@@ -1,35 +1,35 @@
 // login variables
 function SocialShare(div, position) {
-  this.div = div;
-  this.position = position;
-  map.controls[google.maps.ControlPosition[this.position]].push(div);
+    this.div = div;
+    this.position = position;
+    map.controls[google.maps.ControlPosition[this.position]].push(div);
+    setTimeout(toggleSocialLinks, 400);
 }
 
 $(window).on('hashchange', function() {
-    toggleSacialLinks();
+    toggleSocialLinks();
 });
 
-function toggleSacialLinks()
+function toggleSocialLinks()
 {
     console.log(window.location);
     if(window.location.hash) {
-        console.log('have hash, update links to point to: '+window.location.href);
         var map_url_encoded = encodeURIComponent(window.location.href);
         $('#social #facebook').attr(
             'href', 'https://www.facebook.com/sharer/sharer.php?u=' +
-            map_url_encoded);
+                map_url_encoded);
         $('#social #twitter').attr(
             'href',
             'https://twitter.com/home?status='+
-            'My%20trip%20in%20angya%20the%20travel%20planner:%20' +
-            map_url_encoded);
+                'My%20trip%20in%20angya%20the%20travel%20planner:%20' +
+                map_url_encoded);
         $('#social #googleplus').attr(
             'href', 'https://plus.google.com/share?url=' +
-            map_url_encoded);
-        $('#socialmask').addClass('disabled');
+                map_url_encoded);
+        $('#social a').removeClass('disabled');
     }
     else {
         console.log('No hash, means no social sharing');
-        $('#socialmask').removeClass('disabled');
+        $('#social a').addClass('disabled');
     }
 }
